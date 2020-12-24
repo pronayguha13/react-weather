8a07072d5ec1065f83369ed627cd5e958aa18019
pronayguha13 <pronay.guha@aot.edu.in>
2020-12-24 10:05:49 +0530

    Basic card structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ResultArea from "./Components/ResultCard/ResultArea";
 import SearchArea from "./Components/SearchArea/SearchArea";
-
+import styles from "./App.module.css";
 const App = () => {
   return (
-    <div>
+    <div className={styles.App}>
       <h1>Weather App</h1>
       <SearchArea />
       <ResultArea />
