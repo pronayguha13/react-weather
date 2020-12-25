9246e2b42078f657075a00a2c4653c64df88b169
pronayguha13 <pronay.guha@aot.edu.in>
2020-12-25 22:07:54 +0530

    state for location input & function defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import ResultArea from "./Components/ResultCard/ResultArea";
 import SearchArea from "./Components/SearchArea/SearchArea";
 import styles from "./App.module.css";
 const App = () => {
+  const [location, setLocation] = useState("");
+
+  const locationChangeHandler = (e) => {
+    if (e.key === "Enter") {
+      //call to fetch the API result
+    } else {
+      setLocation(e.target.value);
+    }
+  };
   return (
     <div className={styles.App}>
       <div className={styles.header}>
@@ -10,7 +19,10 @@ const App = () => {
           <img src="/icons/icon.png" alt="weather_icon" /> Weather App
         </h1>
       </div>
-      <SearchArea />
+      <SearchArea
+        location={location}
+        locationChangeHandler={locationChangeHandler}
+      />
       <ResultArea />
     </div>
   );
--- src/Components/SearchArea/SearchArea.jsx
+++ src/Components/SearchArea/SearchArea.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./SearchArea.module.css";
-const SearchArea = () => {
+const SearchArea = (props) => {
+  const { location, locationChangeHandler } = props;
   return (
     <div className={styles.locationInputForm}>
       <form action="submit">
@@ -8,6 +9,10 @@ const SearchArea = () => {
           type="text"
           placeholder="Enter your pin-code or city"
           autoFocus
+          value={location}
+          onChange={(e) => locationChangeHandler(e)}
+          onKeyDown={(e) => locationChangeHandler(e)}
+          // add Enter key press handler
         />
       </form>
     </div>
